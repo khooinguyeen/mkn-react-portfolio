f2a605150fcac09681d71787ad4eaf48e19e60a6
khooinguyeen <khoinguyenmai17102005@gmail.com>
2022-08-23 22:57:20 +0700

    add home icon

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons' 
 
 const Sidebar = () => (
     <div className="nav-bar">
@@ -9,6 +11,11 @@ const Sidebar = () => (
             <img src={LogoS} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
         </Link>
+        <nav>
+            <NavLink exact="true" activeClassName="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+        </nav>
     </div>
 )
 
