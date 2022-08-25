40700a380dd522fa3f74670ee485e101e604d31a
khooinguyeen <khoinguyenmai17102005@gmail.com>
2022-08-25 20:51:25 +0700

    complete home section

---
+++ src/components/AnimatedLetters/index.js
@@ -0,0 +1,17 @@
+import './index.scss';
+
+const AnimatedLetters = ({ letterClass, strArray, idx }) => {
+    return (
+        <span>
+            {
+                strArray.map((char, i) => (
+                    <span key={char+1} className={`${letterClass} _${i + idx}`}>
+                        {char}
+                    </span>
+                ))
+            }
+        </span>
+    )
+};
+
+export default AnimatedLetters
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,37 @@
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 const Home = () => {
     
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
     return (
         <div className="container home-page">
-            <div className="text-xone">
-                <h1>Hi, </h1>
+            <div className="text-zone">
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>    
+                <br />
+                <span className={`${letterClass} _13`}>I</span>    
+                <span className={`${letterClass} _13`}>'m</span>    
+                <img src={LogoTitle} alt="developer" />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15} />
+                <br />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={22} />
+                </h1>
+                <h2>Frontend Developer / JavaScript Expert / Youtuber</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
         </div>
-    )
+    );
 }
 
 export default Home
