6ccb18f44016dfb9d0050f3c70f71c217691b40e
khooinguyeen <khoinguyenmai17102005@gmail.com>
2022-09-27 22:11:03 +0700

    mobile devices optimization

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,53 +4,72 @@ import LogoMKN from '../../assets/images/CHUOI 5.png'
 // import LogoS from '../../assets/images/logo-s.png'
 // import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons' 
+import { faBars, faClose, faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons' 
 import { faLinkedin, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
-const Sidebar = () => (
-    <div className="nav-bar">
-        <Link className="logo" to="/">
-            <img src={LogoMKN} alt="logo" />
-            {/* <img className="sub-logo" src={LogoSubtitle} alt="slobodan" /> */}
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </NavLink>
-            <NavLink activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-            </NavLink>
-            <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
-                <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-            </NavLink>
-            <NavLink activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </NavLink>
-        </nav>
+const Sidebar = () => {
+    const [showNav, setShowNav] = useState(false);
 
-        <ul>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://www.google.com.vn/?hl=vi'>
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://www.google.com.vn/?hl=vi'>
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://www.google.com.vn/?hl=vi'>
-                    <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://www.google.com.vn/?hl=vi'>
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-                </a>
-            </li>
-        </ul>
-    </div>
-)
+    return (
+        <div className="nav-bar">
+            <Link className="logo" to="/"
+            onClick={() => setShowNav(false)}>
+                <img src={LogoMKN} alt="logo" />
+                {/* <img className="sub-logo" src={LogoSubtitle} alt="slobodan" /> */}
+            </Link>
+            <nav className={showNav ? 'mobile-show' : ''}>
+                <NavLink exact="true" activeclassname="active" to="/" onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                <NavLink activeclassname="active" className="about-link" to="/about" onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink activeclassname="active" className="portfolio-link" to="/portfolio" onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+                </NavLink>
+                <NavLink activeclassname="active" className="contact-link" to="/contact" onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+                <FontAwesomeIcon 
+                    onClick={() => setShowNav(false)}
+                    icon={faClose}
+                    color="#ffd700"
+                    size='4x'
+                    className='close-icon'
+                />
+            </nav>
 
+            <ul>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://www.google.com.vn/?hl=vi'>
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://www.google.com.vn/?hl=vi'>
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://www.google.com.vn/?hl=vi'>
+                        <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://www.google.com.vn/?hl=vi'>
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
+            <FontAwesomeIcon
+                onClick={() => setShowNav(true)}
+                icon={faBars}
+                color="#ffd700"
+                size="3x"
+                className="hamburger-icon"
+            />
+        </div>
+    )
+}
 export default Sidebar
