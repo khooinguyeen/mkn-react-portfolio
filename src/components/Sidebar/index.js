bcbc8399be1981b5ada907a5a4ac0c2e913565fb
Khôi Nguyên <khoinguyenmai17102005@gmail.com>
2025-11-28 19:27:09 +1030

    fix: Fix responsiveness for mobile display

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -31,10 +31,9 @@ const Sidebar = () => {
                 <NavLink activeclassname="active" className="contact-link" to="/contact" onClick={() => setShowNav(false)}>
                     <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
-                <FontAwesomeIcon 
+                <FontAwesomeIcon
                     onClick={() => setShowNav(false)}
                     icon={faClose}
-                    color="#ED6165"
                     size='4x'
                     className='close-icon'
                 />
@@ -65,7 +64,6 @@ const Sidebar = () => {
             <FontAwesomeIcon
                 onClick={() => setShowNav(true)}
                 icon={faBars}
-                color="#ED6165"
                 size="3x"
                 className="hamburger-icon"
             />
