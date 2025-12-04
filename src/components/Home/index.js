e87b7121623c63b4e4507109c17440aaadb4b518
Khôi Nguyên <khoinguyenmai17102005@gmail.com>
2025-12-04 16:25:29 +1030

    fix(Homepage): fix horizontal scroll bug

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,7 +6,6 @@ import myPicture from '../../assets/images/me.jpg'
 import './index.scss'
 import Loader from 'react-loaders'
 const Home = () => {
-    
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = [' ','N','g','u','y','e','n',' ','(','J','u','l','i','a','n',')']
     const jobArray = [
@@ -18,7 +17,6 @@ const Home = () => {
         'a',
         'r',
         'e',
-        ' ',
         'e',
         'n',
         'g',
@@ -35,7 +33,82 @@ const Home = () => {
         }, 4000);
         return () => clearTimeout(timer);
       }, []);
+    
+    if (window.innerWidth <= 600) {
+        return (
+            <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _1`}>i</span>
+                        <span className={`${letterClass} _2`}>,</span>       
+                        <br />
+                        <span className={`${letterClass} _3`}>I</span>    
+                        <span className={`${letterClass} _4`}>'</span>  
+                        <span className={`${letterClass} _5`}>m</span>    
+                        {/* <img src={LogoTitle} alt="developer" /> */}
+                        <span className={`${letterClass} _6`}> </span>
+                        <span className={`${letterClass} _7`}>N</span>
+                        <span className={`${letterClass} _8`}>g</span> 
+                        <span className={`${letterClass} _9`}>u</span>  
+                        <span className={`${letterClass} _10`}>y</span>  
+                        <span className={`${letterClass} _11`}>e</span>  
+                        <span className={`${letterClass} _12`}>n</span>  
+                        <br />
+                        <span className={`${letterClass} _13`}>(</span>  
+                        <span className={`${letterClass} _14`}>J</span>  
+                        <span className={`${letterClass} _15`}>u</span>  
+                        <span className={`${letterClass} _16`}>l</span>  
+                        <span className={`${letterClass} _17`}>i</span>  
+                        <span className={`${letterClass} _18`}>a</span>  
+                        <span className={`${letterClass} _19`}>n</span>  
+                        <span className={`${letterClass} _20`}>)</span>                         
+                        <br />
+                        <span className={`${letterClass} _21`}>S</span>  
+                        <span className={`${letterClass} _22`}>o</span>  
+                        <span className={`${letterClass} _23`}>f</span>  
+                        <span className={`${letterClass} _24`}>t</span>  
+                        <span className={`${letterClass} _25`}>w</span>  
+                        <span className={`${letterClass} _26`}>a</span>  
+                        <span className={`${letterClass} _27`}>r</span>  
+                        <span className={`${letterClass} _28`}>e</span>   
+                        <br />
+                        <span className={`${letterClass} _29`}>E</span>  
+                        <span className={`${letterClass} _30`}>n</span>  
+                        <span className={`${letterClass} _31`}>g</span>  
+                        <span className={`${letterClass} _32`}>i</span>  
+                        <span className={`${letterClass} _33`}>n</span>  
+                        <span className={`${letterClass} _34`}>e</span>  
+                        <span className={`${letterClass} _35`}>e</span>  
+                        <span className={`${letterClass} _36`}>r</span>   
 
+                    </h1>
+                    <h2>a Computer Science student who is passionate about programming, especially using it to solve problem in life</h2>
+                    <a
+                        href="/mkn-react-portfolio/Khoi_Nguyen_Mai_Resume.pdf"
+                        download="Nguyen_Julian_CV.pdf"
+                        className='flat-button'
+                    >
+                        DOWNLOAD CV
+                    </a>
+                </div>
+
+            </div>
+            <div className='picture-container'>
+                <div className='image-wrapper'>
+                    <img src={myPicture} alt='me'></img>
+                    <div className='dot dot-1'></div>
+                    <div className='dot dot-2'></div>
+                    <div className='dot dot-3'></div>
+                </div>
+            </div>
+            <Loader type='ball-clip-rotate-multiple' />
+            {/* <Loader type='cube-transition' /> */}
+            {/* <Loader type='line-scale-pulse-out' /> */}
+            </>
+        )
+    }
     return (
         <>
         <div className="container home-page">
@@ -70,8 +143,6 @@ const Home = () => {
                     DOWNLOAD CV
                 </a>
             </div>
-            {/* <Logo /> put image here */}
-            {/* <img src={myPicture} alt="tui"/> */}
 
         </div>
         <div className='picture-container'>
